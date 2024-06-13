fixed normalization of variances, made integration time calculated by the azimuth resolution and not the opposite. added spatial resolution

SAR_imaging.py
@@ -3,7 +3,8 @@ from numpy.fft import fft2, fftshift, ifft2, ifftshift
 from enum import Enum
 from omnidirectional_spectrum import omnidirectional_spectrum, spectrum_model
 from scipy import special
-
+from parameters import parameters
+from surface import surfaceGenerator
 class Polarization(Enum):
     Vertical=0
     Horizontal=1
@@ -15,36 +16,40 @@ class Band(Enum):
     L=2
 
 class SAR_imaging:
-    def __init__(self, surfaceGenerator, length, N, spectrum, incidence_angle, wind_speed, wind_direction, fetch, spatial_resolution, integration_time):
+    def __init__(self, surfaceGenerator:surfaceGenerator, params:parameters):
         self.random_phase=surfaceGenerator.random_phase
         self.PSI=surfaceGenerator.PSI
         self.wave_coeffs=surfaceGenerator.wave_coeffs
         self.polarization=Polarization.Vertical
-        self.H=693000
-        self.V=7400
-        self.R=self.H/np.cos(incidence_angle)
-        self.beta=1#self.R/self.V
-        self.spatial_resolution=spatial_resolution
+        self.H=800000
+        self.V=7000
+        self.R=self.H/np.cos(params.incidence_angle)
+        self.beta=self.R/self.V
+        if params.beta!=None:
+            self.beta=params.beta
         print(self.beta)
-        self.L=length
-        self.N=N
-        self.dx=length/N
+        self.L=params.length
+        self.N=params.N
+        self.dx=params.length/params.N
         self.surface=surfaceGenerator.surface[0,:,:]
-        self.wind_speed=wind_speed
-        self.wind_direction=wind_direction
-        self.fetch=fetch
-        self.incidence_angle=incidence_angle
-        self.integration_time=integration_time
+        self.wind_speed=params.wind_speed
+        self.wind_direction=params.wind_direction
+        self.fetch=params.fetch
+        self.incidence_angle=params.incidence_angle
+        self.azimuth_resolution=params.azimuth_resolution
+        self.range_resolution=params.range_resolution
+        self.spatial_resolution=self.azimuth_resolution*self.range_resolution
         self.ky=surfaceGenerator.KY#*np.sin(np.pi/2)#*np.sin(np.linspace(0,2*np.pi, self.N))#wavenumbers*np.sin((np.linspace(0, 2*np.pi,(N))))
         self.kx=surfaceGenerator.KX#*np.cos(np.pi/2)#np.cos(np.linspace(0,2*np.pi, self.N))#wavenumbers*np.cos((np.linspace(0, 2*np.pi,(N))))
         self.wavenumbers=surfaceGenerator.K
-        self.theta=np.linspace(0, 2*np.pi, (N))
-        self.spectrum=spectrum
+        self.theta=np.linspace(0, 2*np.pi, (params.N))
+        self.spectrum=params.spectrum
         self.dispersion_relation=surfaceGenerator.omega
         self.hydrodynamic_relaxation_rate=0.5
         self.light_speed=299792458
-        self.frequency=5e9
+        self.frequency=params.frequency
         self.wavelength=self.light_speed/self.frequency
+        self.integration_time=(self.wavelength*self.beta)/(2*self.azimuth_resolution)#integration_time
         self.k_e=2*np.pi/(self.light_speed/self.frequency)
         self.band=Band.C
         self.I=np.zeros((self.N,self.N))
@@ -57,6 +62,7 @@ class SAR_imaging:
         self.Ks=surfaceGenerator.Ks
 
     def generate(self):
+        self.NRCS()
         self.orbital_velocity()
         # self.orbital_velocity_sum()
         self.image()
@@ -66,7 +72,6 @@ class SAR_imaging:
 
     def image(self):
         x, y=np.meshgrid(np.linspace(0, self.L, self.N), np.linspace(0, self.L, self.N))
-        sigma=self.NRCS()
         Ur=self.u_r
         print(f"Variance of Obrital Velocities {np.var(Ur)}")
         # print(f"rho0 {self.v_covariance(0)[0,0]} max {np.max(self.v_covariance(0))}")
@@ -76,7 +81,7 @@ class SAR_imaging:
         # print(f"rho a {pa}")
         for i in range(self.N):
             for j in range(self.N):
-                self.I[i, j]=np.pi*self.integration_time**2*self.azimuth_resolution()/2*np.trapz(sigma[i,:]/pa*np.exp(-(np.pi/pa)**2*((x[i,j]-x[i,:]-self.beta*Ur[i,:]))**2), dx=self.dx*self.dx)
+                self.I[i, j]=np.pi*self.integration_time**2*self.azimuth_resolution/2*np.trapz(self.sigma[i,:]/pa*np.exp(-(np.pi/pa)**2*((x[i,j]-x[i,:]-self.beta*Ur[i,:]))**2), dx=self.dx*self.dx)
                 # print(f"{i},{j} {self.I[i,j]}")
         # print(f"sigma {self.I}")
     
@@ -91,12 +96,10 @@ class SAR_imaging:
         sn=0#np.gradient(self.surface,axis=0)
         sp=0#np.gradient(self.surface,axis=1)
         theta_l=np.arccos(np.cos(theta-sp)*np.cos(sn))
-        print(f"psi {np.cos(theta_l)}")
         return 8*np.pi*self.k_e**4*np.cos(theta_l)**4*PSI*np.abs(self.complex_scattering())**2
     
     def NRCS(self):
-        print(f"average nrcs {self.average_NRCS()}")
-        return self.average_NRCS()*(1+np.real(2*ifft2(ifftshift(self.RAR_MTF()*fftshift(fft2(self.surface))+np.conj(self.RAR_MTF()*fftshift(fft2(self.surface)))))))
+        self.sigma=self.average_NRCS()*(1+np.real(2*ifft2(ifftshift(self.RAR_MTF()*fftshift(fft2(self.surface))+np.conj(self.RAR_MTF()*fftshift(fft2(self.surface)))))))
 
     def complex_scattering(self):
         if self.polarization==Polarization.Vertical:
@@ -125,7 +128,6 @@ class SAR_imaging:
     
     def orbital_velocity(self):
         self.u_r=np.real(ifft2(ifftshift(self.orbital_velocity_mtf()*fftshift(fft2(self.surface)))))
-        return
 
     def orbital_velocity_sum(self):
         x, y=np.meshgrid(np.linspace(-self.L/2,self.L/2,self.N), np.linspace(-self.L/2,self.L/2,self.N))
@@ -171,14 +173,11 @@ class SAR_imaging:
 
     def coherence_time(self):
         wind_speed_19_5=self.wind_speed*(19.5/10)**(1/7)
-        print(f"wind speed {wind_speed_19_5}")
-        return 3*self.wavelength/wind_speed_19_5*special.erf(2.7*self.azimuth_resolution()/wind_speed_19_5**2)**(-1/2)
+        return 3*self.wavelength/wind_speed_19_5*special.erf(2.7*self.spatial_resolution/wind_speed_19_5**2)**(-1/2)
     
-    def azimuth_resolution(self):
-        lamda_e=2*np.pi/self.k_e
-        # print(f"azimuth resolution {(lamda_e*self.R)/(2*self.V*self.integration_time)}")
-        return 1
-        return (lamda_e*self.R)/(2*self.V*self.integration_time)
+    # def azimuth_resolution(self):
+    #     return 5
+    #     return (self.wavelength*self.beta)/(2*self.integration_time)
 
     def degraded_azimuthal_resolution(self):
         # print(np.sqrt(1+self.integration_time**2/self.coherence_time()**2))
@@ -187,53 +186,33 @@ class SAR_imaging:
         print(f"integration time {self.integration_time}")
         print(f"coherence time {self.coherence_time()}")
         # return 2
-        return self.azimuth_resolution()*np.sqrt(1+(self.integration_time/self.coherence_time())**2)
+        return self.azimuth_resolution*np.sqrt(1+(self.integration_time/self.coherence_time())**2)
     
-    def wave_field(self):
-        return 2*np.abs(self.SAR_MTF())**2*fftshift(fft2(self.I))
-        return np.abs(fftshift(fft2(self.surface))) #1/2*np.real(2*np.abs(self.SAR_MTF())**2*fftshift(fft2(self.I)))
-
     def noisy_image(self):
         noise=np.random.exponential(scale=1,size=(self.N, self.N))
         return self.I*noise
     
     def v_covariance(self):
-        self.f_v=np.real(ifft2(ifftshift(abs(self.orbital_velocity_mtf())**2*self.PSI/self.Ks)))
-        # self.f_v=np.real(ifft2(ifftshift(self.N*self.N*abs(self.orbital_velocity_mtf())**2*self.PSI/self.Ks)))#,(self.N,self.N),norm='ortho')/(np.pi*self.dx)**2).astype(np.float32)
-        # self.f_vA=np.real(ifft2(ifftshift(abs(self.orbital_velocity_mtf())**2*self.PSI/self.Ks),(self.N,self.N)))
-        # return np.trapz(np.trapz((abs(self.orbital_velocity_mtf()))**2*self.PSI,self.kx[0,:],axis=0),self.ky[:,0],axis=0)*np.exp(1j*self.wavenumbers*np.sqrt(x))
+        self.f_v=np.real(ifft2(ifftshift(abs(self.orbital_velocity_mtf())**2*self.PSI*(2*np.pi/self.dx)**2)))
 
     def R_covariance(self):
-        self.f_r=0.5*np.real(ifft2(ifftshift(abs(self.RAR_MTF())**2*self.PSI/self.Ks+abs(np.rot90(self.RAR_MTF(),2))**2*np.rot90(self.PSI/self.Ks,2))))
+        self.f_r=0.5*np.real(ifft2(ifftshift(abs(self.RAR_MTF())**2*self.PSI*(2*np.pi/self.dx)**2+abs(np.rot90(self.RAR_MTF(),2))**2*np.rot90(self.PSI*(2*np.pi/self.dx)**2,2))))
         # return 1/2*np.trapz(np.trapz((abs(self.RAR_MTF())**2*self.PSI+np.conj(abs(self.RAR_MTF())**2*self.PSI))*np.exp(1j*self.wavenumbers*x),self.wavenumbers[0,:], axis=0),self.wavenumbers[:,0])
 
     def Rv_covariance(self):
-
-        self.f_rv=0.5*np.real(ifft2(ifftshift(self.RAR_MTF()*np.conj(self.orbital_velocity_mtf())*self.PSI/self.Ks+np.conj(np.rot90(self.RAR_MTF(),2))*np.rot90(self.orbital_velocity_mtf(),2)*np.rot90(self.PSI/self.Ks,2))))
+        self.f_rv=0.5*np.real(ifft2(ifftshift(self.RAR_MTF()*np.conj(self.orbital_velocity_mtf())*self.PSI*(2*np.pi/self.dx)**2+np.conj(np.rot90(self.RAR_MTF(),2))*np.rot90(self.orbital_velocity_mtf(),2)*np.rot90(self.PSI*(2*np.pi/self.dx)**2,2))))
         # return 1/2*np.trapz(np.trapz((self.RAR_MTF()*self.orbital_velocity_mtf()*self.PSI+np.conj(self.RAR_MTF()*self.orbital_velocity_mtf()*self.PSI))*np.exp(1j*self.wavenumbers*x),self.wavenumbers[0,:], axis=0),self.wavenumbers[:,0])
 
-    def inverse_linear_transform(self):
+    def linear_mapping_transform(self):
         return 0.5*(abs(self.SAR_MTF())**2*self.PSI+(abs(np.rot90(self.SAR_MTF(),2))**2*np.rot90(self.PSI,2)))
     
-    def inverse_linear_transformRAR(self):
-        return 0.5*(abs(self.RAR_MTF())**2*self.PSI+(abs(np.rot90(self.RAR_MTF(),2))**2*np.rot90(self.PSI,2)))
-
-    def inverse_quasilinear_transform(self):
+    def quasilinear_mapping_transform(self):
         # i think i might be omiting the imag part
         # print(f"rho0 {self.v_covariance(0)}")
         x, y=np.meshgrid(np.linspace(0, self.L, self.N), np.linspace(0, self.L, self.N))
-        return np.exp(-self.kx**2*self.beta**2*self.f_v[0,0])*self.inverse_linear_transform()
+        return np.exp(-self.kx**2*self.beta**2*self.f_v[0,0])*self.linear_mapping_transform()
     
-    def inverse_quasilinear_transformRAR(self):
-        # i think i might be omiting the imag part
-        # print(f"rho0 {self.v_covariance(0)}")
-        return np.exp(-self.kx**2*self.beta**2*self.f_v[0,0])*self.inverse_linear_transformRAR()
-
-    def oldInverse_nonlinear_transform(self):
-        kx=fftshift(self.kx)
-        return fftshift((2*np.pi)**(-2)*np.exp(-kx**2*self.beta**2*self.f_v[0,0])*(fft2(np.exp(kx**2*self.beta**2*self.f_v)*(1+self.f_r+1j*kx*self.beta*(self.f_rv-np.rot90(self.f_rv))+(kx*self.beta)**2*(self.f_rv-self.f_rv[0,0])*(np.rot90(self.f_rv,2)-self.f_rv[0,0])))))
-
-    def inverse_nonlinear_transform(self, n):
+    def nonlinear_mapping_transform(self, n):
         nonLinearTerms=np.zeros((self.N, self.N)).astype(np.complex64)
         for i in range(1,n+1):
             nonLinearTerms+=self.nonLinearity(i)
@@ -251,3 +230,25 @@ class SAR_imaging:
         else:
             factorial_2=1/math.factorial(n-2)
         return (2*np.pi)**(-2)*fftshift(fft2(1/math.factorial(n-1)*self.f_r*self.f_v**(n-1)+factorial_2*(self.f_rv-self.f_rv[0,0])*(np.rot90(self.f_rv,2)-self.f_rv[0,0])*self.f_r**(n-2)))
+    
+    def autocovariance(self, image, lag_x=0, lag_y=0):
+        rows, cols = image.shape
+        mean_image = np.mean(image)
+        autocov = 0.0
+        for i in range(rows - lag_y):
+            for j in range(cols - lag_x):
+                autocov += (image[i, j] - mean_image) * (image[i + lag_y, j + lag_x] - mean_image)
+        autocov /= ((rows - lag_y) * (cols - lag_x))
+        
+        return autocov
+    
+    def crosscovariance(self, image1, image2, lag_x=0, lag_y=0):
+        rows, cols = image1.shape
+        mean_image1 = np.mean(image1)
+        mean_image2 = np.mean(image2)
+        cov = 0.0
+        for i in range(rows - abs(lag_y)):
+            for j in range(cols - abs(lag_x)):
+                cov += (image1[i, j] - mean_image1) * (image2[i + lag_y, j + lag_x] - mean_image2)
+        return cov / ((rows - abs(lag_y)) * (cols - abs(lag_x)))
+        

simulation.py
@@ -3,26 +3,12 @@ from omnidirectional_spectrum import spectrum_model
 from spreading_function import spreading_model
 import numpy as np
 from plots import plotMTFs, animate, plotModulations, plotSpectras, plotSurfaceSAR
-spectrum=spectrum_model.Elfouhaily
-spreading=spreading_model.Longuet_Higgins
+from parameters import parameters
 
-    
-
-n=6
-S=12
-length=256
-N=256
-wind_speed=5
-wind_direction=np.pi/2
-seconds=5
-timestep=0.5
-fetch=25000
-elfouhaily_k=0.1
-spatial_resolution=2
-integration_time=0.5
+params=parameters()
 #%% Surface Generation
 from surface import surfaceGenerator
-surfaceGenerator=surfaceGenerator(spectrum, spreading, length, N, wind_speed, wind_direction, n, S, seconds, timestep, fetch, elfouhaily_k)
+surfaceGenerator=surfaceGenerator(params)
 Z=surfaceGenerator.generate()
 animate(Z)
 print(f"Surface Variances {surfaceGenerator.getSurfaceVariances()}")
@@ -33,10 +19,10 @@ print(f"Significant wave height {surfaceGenerator.getSignificantWaveHeights()}")
 print(f"max {np.max(Z[0,:,:])}")
 #%% Sar Imaging
 from SAR_imaging import SAR_imaging
-sar=SAR_imaging(surfaceGenerator, length, N,spectrum_model.Pierson_Moskowitz, np.deg2rad(35), wind_speed, wind_direction, fetch, spatial_resolution, integration_time)
+sar=SAR_imaging(surfaceGenerator, params)
 sar.generate()
 plotMTFs(sar)
-plotModulations(sar)
+# plotModulations(sar)
 plotSpectras(sar)
 plotSurfaceSAR(sar)
 integral_covariance=np.trapz(np.trapz((abs(sar.orbital_velocity_mtf()))**2*sar.PSI,sar.kx[0,:],axis=0),sar.ky[:,0],axis=0)
@@ -45,14 +31,14 @@ integral_covariance=np.trapz(np.trapz((abs(sar.orbital_velocity_mtf()))**2*sar.P
 
 plt.legend()
 #------Orbital Velocity Verification-------
-# # # # fig2, (axa,axb,axOV, axOVsum)=plt.subplots(1,4)
-# # # # surfaceColorbar=axa.imshow(sar.surface, origin='lower')
-# # # # plt.colorbar(surfaceColorbar, ax=axa)
-# # # # etaColorbar=axb.imshow(sar.hta, origin='lower')
-# # # # plt.colorbar(etaColorbar, ax=axb)
-# # # # ovColorbar=axOV.imshow(sar.u_r, origin='lower')
-# # # # plt.colorbar(ovColorbar, ax=axOV)
-# # # # ovSumColorbar=axOVsum.imshow(sar.u_r_sum, origin='lower')
-# # # # plt.colorbar(ovSumColorbar, ax=axOVsum)
-# # # # print(f"{np.var(sar.surface)} {np.var(sar.hta)} {np.var(sar.u_r)} {np.var(sar.u_r_sum)}")
+# fig2, (axa,axb,axOV, axOVsum)=plt.subplots(1,4)
+# surfaceColorbar=axa.imshow(sar.surface, origin='lower')
+# plt.colorbar(surfaceColorbar, ax=axa)
+# etaColorbar=axb.imshow(sar.hta, origin='lower')
+# plt.colorbar(etaColorbar, ax=axb)
+# ovColorbar=axOV.imshow(sar.u_r, origin='lower')
+# plt.colorbar(ovColorbar, ax=axOV)
+# ovSumColorbar=axOVsum.imshow(sar.u_r_sum, origin='lower')
+# plt.colorbar(ovSumColorbar, ax=axOVsum)
+# print(f"{np.var(sar.surface)} {np.var(sar.hta)} {np.var(sar.u_r)} {np.var(sar.u_r_sum)}")
 plt.show()

surface.py
@@ -3,31 +3,32 @@ import numpy as np
 from numpy.fft import fft2, fftshift, ifft2, ifftshift, fftfreq
 from omnidirectional_spectrum import omnidirectional_spectrum, spectrum_model
 from spreading_function import spreading_function, spreading_model
-
+from parameters import parameters
 class surfaceGenerator:
-    def __init__(self, spectrum, spreading, length, N, wind_speed, wind_direction, n, S, seconds, timestep, fetch, elfouhaily_k):
-        self.spectrum=spectrum
+    def __init__(self, params:parameters):
+        self.spectrum=params.spectrum
         self.omnidirectional_spectrum=None
         self.spreading_function=None
-        self.spreading=spreading
+        self.spreading=params.spreading
         self.g=9.81
-        self.dx=length/N
-        self.L=length
-        self.N=N
-        self.n=n
-        self.S=S
-        self.elfouhaily_k=elfouhaily_k
+        self.dx=params.length/params.N
+        self.L=params.length
+        self.N=params.N
+        self.n=params.n
+        self.S=params.S
+        self.elfouhaily_k=params.elfouhaily_k
         print(self.N)
         print(self.dx)
         print(self.L)
         self.x=np.linspace(-self.L/2,self.L/2,self.N)
         self.y=np.linspace(-self.L/2,self.L/2,self.N)
-        self.time=np.linspace(0,seconds, int(seconds/timestep))
+        self.time=np.linspace(0,params.seconds, int(params.seconds/params.timestep))
         self.surface=np.zeros((self.time.size, self.N, self.N))
-        self.wind_speed=wind_speed
-        self.wind_direction=wind_direction
-        self.fetch=fetch
-        
+        self.wind_speed=params.wind_speed
+        self.wind_direction=params.wind_direction
+        self.fetch=params.fetch
+        self.MONOCHROMATIC=params.MONOCHROMATIC
+
     
     def generateSurface(self):
         kx_s = fftshift((2*np.pi*fftfreq(self.N, self.dx)).astype(np.float32))
@@ -36,14 +37,14 @@ class surfaceGenerator:
         kx_res = kx[0, 1] - kx[0, 0]
         ky_res = ky[1, 0] - ky[0, 0]
         self.Ks=kx_res
-        MONOCHROMATIC=False
-        if MONOCHROMATIC:
+        print(f"kx_s {kx_s[1]-kx_s[0]}, kx_res {kx_res}")
+        if self.MONOCHROMATIC:
             x=self.N//2+10
             y=self.N//2
             tmp=kx[x,y]
             kx=np.zeros((self.N,self.N))
             ky=np.zeros((self.N,self.N))
-            kx[x,y]=0.5
+            kx[x,y]=0.01
             ky[x,y]=0
         self.KX=kx
         self.KY=ky